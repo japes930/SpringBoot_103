dc3f4a28bbc3352c169a0f344fcb85f909caed43
japes930 <jpvillaflores9@gmail.com>
2019-06-12 16:44:55 -0400

    Added comments to Input Output assignment

--- Input_Output_Assn/src/InputOutput.java
+++ Input_Output_Assn/src/InputOutput.java
@@ -3,18 +3,20 @@ public class InputOutput
 {
     public static void main( String[] args )
     {
+        //Read in Scanner class from library and sets to keyboard variable
         Scanner keyboard = new Scanner(System.in);
 
+        //Declares variables
         double num1, num2, num3;
 
+        //Asks user for 2 temperatures
         System.out.print( "First temperature? " );
         num1 = keyboard.nextDouble();
-
         System.out.print( "Second temperature? " );
         num2 = keyboard.nextDouble();
 
+        //Adds first two numbers, gets average, then prints avg out
         num3 = (Double.sum(num1, num2)) / 2;
-
         System.out.println("The average temperature is : " + num3);
     }
 }
