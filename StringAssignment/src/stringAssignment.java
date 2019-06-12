9f7b4538ddd68725ed06773b78c35d36dbdb9eae
japes930 <jpvillaflores9@gmail.com>
2019-06-12 15:31:16 -0400

    String Exercise

---
+++ StringAssignment/src/stringAssignment.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class stringAssignment {
+    public static void main (String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        String name;
+        System.out.println("What is your name");
+        name = keyboard.nextLine();
+        System.out.println(name);
+
+    }
+}
