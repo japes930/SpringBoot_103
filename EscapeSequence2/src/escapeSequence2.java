f61f8d103fe4378931ee9d4de00fb3657a46c5e0
japes930 <jpvillaflores9@gmail.com>
2019-06-13 10:42:53 -0400

    EscapeSequence II

---
+++ EscapeSequence2/src/escapeSequence2.java
@@ -0,0 +1,9 @@
+public class escapeSequence2 {
+    public static void main (String[] args){
+        System.out.println("Cost\t\tQuantity\t\tTotal\n$1,000.00\t\t4\t\t\t$4,000.00\n" +
+                "$\t50.00\t\t8\t\t\t\t$400.00\n" +
+                "______________________________________\n" +
+                "TOTAL:\t\t\t12\t\t\t$4,400.00");
+    }
+
+}
