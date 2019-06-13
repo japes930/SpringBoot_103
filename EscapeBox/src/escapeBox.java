d17abfc7ca24984bf48a1889ced8d4e0e72f57e7
japes930 <jpvillaflores9@gmail.com>
2019-06-13 10:14:06 -0400

    Escape Box

---
+++ EscapeBox/src/escapeBox.java
@@ -0,0 +1,7 @@
+public class escapeBox {
+    public static void main(String [] args) {
+    String txt = "*********\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n*********";
+    System.out.println(txt);
+
+    }
+}
