9a2e778b4b9742cda22aa920e6a325b7d3973440
japes930 <jpvillaflores9@gmail.com>
2019-07-08 17:03:01 -0400

    added images

---
+++ hobbies/src/main/java/com/example/hobbies/HobbiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.hobbies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbiesApplication.class, args);
+	}
+
+}
